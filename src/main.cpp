cee15ccde408776790207dc6a45b48ee73751e24
Ravil <Sergey_esenin1@mail.ru>
2022-12-05 10:59:37 +0300

    added main.cpp and CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//#include<glad.h>
+//#include<glfw3.h>
+#include<iostream>
+int main() {
+   // glfwInit();
+   // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+   // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   ///* glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
+   // GLFWwindow* window = glfwCreateWindow(800, 800, "blabla", NULL, NULL);
+   // if (!window) {
+   //     std::cout << "Fail, but dont give up anyway!!!" << std::endl;
+   //     glfwTerminate();
+   //     return -1;
+   // }
+   // while (!glfwWindowShouldClose(window)) {
+   //     glfwPollEvents();
+   // }
+   // glfwMakeContextCurrent(window);
+   // glfwDestroyWindow(window);
+   // glfwTerminate();
+	std::cout << "blsblsbla" << std::endl;
+	return 0;
+}
